126bc895166034346d30e99733f531247bdba76c
charlesrolandscr17 <charlesrolandscr17@gmail.com>
2024-09-20 10:15:24 +0300

    project structure

---
+++ Group_BSE25_34_Capstone/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Group_BSE25_34_Capstone project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Group_BSE25_34_Capstone.settings')
+
+application = get_asgi_application()
