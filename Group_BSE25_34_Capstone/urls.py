1dca61436bb85049b7cc5b43bdb8d72da8f304f7
Wafula-Ian01 <wafula-ian01@github.com>
2024-10-03 09:45:07 +0300

    Template Updates 1

--- Group_BSE25_34_Capstone/urls.py
+++ Group_BSE25_34_Capstone/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from price_aggregator.views import ProductListView 
+from price_aggregator.views import ProductListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('price_aggregator/', include('price_aggregator.urls')),
-    path('', ProductListView.as_view(), name='home'),  # Root URL redirects to product list
+    # Root URL redirects to product list
+    path('', ProductListView.as_view(), name='home'),
 ]
--- price_aggregator/migrations/0001_initial.py
+++ price_aggregator/migrations/0001_initial.py
@@ -14,10 +14,21 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Product',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=100)),
-                ('price', models.DecimalField(decimal_places=2, max_digits=10)),
-                ('description', models.TextField()),
+                ('id',
+                 models.BigAutoField(
+                     auto_created=True,
+                     primary_key=True,
+                     serialize=False,
+                     verbose_name='ID')),
+                ('name',
+                 models.CharField(
+                     max_length=100)),
+                ('price',
+                 models.DecimalField(
+                     decimal_places=2,
+                     max_digits=10)),
+                ('description',
+                 models.TextField()),
             ],
         ),
     ]
