9ff0bfebe20fd7b8b4cbeafdad01624e4dbc1b07
Wafula-Ian01 <wafula-ian01@github.com>
2024-10-04 13:27:34 +0300

    Modified the wsgi.py

--- Group_BSE25_34_Capstone/wsgi.py
+++ Group_BSE25_34_Capstone/wsgi.py
@@ -8,8 +8,13 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+project_home = '~/Capstone/Group_BSE25-34-Capstone'
+if project_home not in sys.path:
+    sys.path.append(project_home)
 
 os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE',
