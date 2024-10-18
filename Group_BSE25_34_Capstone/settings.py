851530946ac2fbe3a1cd3a287f180a6a12b2a555
charlesrolandscr17 <charlesrolandscr17@gmail.com>
2024-10-18 13:18:13 +0300

    Turned off debug mode for production

--- Group_BSE25_34_Capstone/settings.py
+++ Group_BSE25_34_Capstone/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = (
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'group-bse25-34-capstone-eaas.onrender.com']
 
